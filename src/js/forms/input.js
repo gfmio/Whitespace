461c5b715a7dc8deac5c6083cc695855e856e325
Frédérique Mittelstaedt <frederique@gfm.io>
2016-02-05 06:29:53 +1300

    Switched to rest and spread props transfer

--- src/js/forms/input.js
+++ src/js/forms/input.js
@@ -6,15 +6,16 @@ var React = require('react');
 
 export var Input = React.createClass({
   render: function() {
+    var {type, name, className, value, label, multiple, ...other } = this.props;
+
     if (this.props.type == "textarea") {
       return (
         <div className="input-group">
-          <textarea
+          <textarea { ...other }
             name={ this.props.name }
             placeholder={ this.props.placeholder }
             required={ this.props.required ? true : false }
-            className={ this.props.className + ((this.props.value === "") || (this.props.value == undefined) ? "empty" : "") }
-            { ...this.props } >
+            className={ this.props.className + ((this.props.value === "") || (this.props.value == undefined) ? "empty" : "") } >
             { this.props.value }
           </textarea>
           <label>{ this.props.label }</label>
@@ -25,12 +26,11 @@ export var Input = React.createClass({
     } else if (this.props.type == "select") {
       return (
         <div className="input-group">
-          <select
+          <select { ...other }
             multiple={ this.props.multiple ? true : false }
             name={ this.props.name }
             required={ this.props.required ? true : false }
-            className={ this.props.className + ((this.props.value === "") || (this.props.value == undefined) ? "empty" : "") }
-            { ...this.props } >
+            className={ this.props.className + ((this.props.value === "") || (this.props.value == undefined) ? "empty" : "") }>
             { this.props.children }
           </select>
           <label>{ this.props.label }</label>
@@ -41,14 +41,13 @@ export var Input = React.createClass({
     } else {
       return (
         <div className="input-group">
-          <input
+          <input { ...other }
             type={ this.props.type }
             name={ this.props.name }
             placeholder={ this.props.placeholder }
             value={ this.props.value }
             required={ this.props.required ? true : false }
-            className={ this.props.className + ((this.props.value === "") || (this.props.value == undefined) ? "empty" : "") }
-            { ...this.props } />
+            className={ this.props.className + ((this.props.value === "") || (this.props.value == undefined) ? "empty" : "") } />
           <label>{ this.props.label }</label>
           <p className="hint">{ this.props.hint }</p>
           <span className="material-input"></span>
